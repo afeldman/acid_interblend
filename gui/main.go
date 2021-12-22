45b97738d266d85420f8da736293a9dcef064a55
Anton Feldmann <anton.feldmann>
2021-12-22 15:33:37 +0100

    canot build fyne desktop app with gorelease

--- gui/main.go
+++ gui/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"acid_interblend/acid"
 	"fmt"
-	"log"
-	"reflect"
 	"strconv"
 
 	"fyne.io/fyne/app"
@@ -51,17 +49,17 @@ func main() {
 		widget.NewLabel("Volume S2"), result_solution_2)
 
 	content := container.NewVBox(grid, reslut_row, widget.NewButton("Calc", func() {
-		s1, err := strconv.ParseFloat(input_s1.Text, 64)
-		log.Println(s1, err, reflect.TypeOf(s1))
+		s1, _ := strconv.ParseFloat(input_s1.Text, 64)
+		//log.Println(s1, err, reflect.TypeOf(s1))
 
-		s2, err := strconv.ParseFloat(input_s2.Text, 64)
-		log.Println(s2, err, reflect.TypeOf(s2))
+		s2, _ := strconv.ParseFloat(input_s2.Text, 64)
+		//log.Println(s2, err, reflect.TypeOf(s2))
 
-		rs, err := strconv.ParseFloat(input_result.Text, 64)
-		log.Println(rs, err, reflect.TypeOf(rs))
+		rs, _ := strconv.ParseFloat(input_result.Text, 64)
+		//log.Println(rs, err, reflect.TypeOf(rs))
 
-		vol, err := strconv.ParseFloat(input_volume.Text, 64)
-		log.Println(vol, err, reflect.TypeOf(vol))
+		vol, _ := strconv.ParseFloat(input_volume.Text, 64)
+		//log.Println(vol, err, reflect.TypeOf(vol))
 
 		output_volume_s1, output_volume_s2 := acid.AcidCrossCalculation(s1, s2, rs, vol)
 
